108a39eb2227a3262be4420f9471eb2597932fc4
LuisD48 <156547702+LuisD48@users.noreply.github.com>
2025-05-21 20:53:20 -0700

    Create Datos_Empleados.sql

---
+++ Datos_Empleados.sql
@@ -0,0 +1,5 @@
+SELECT * FROM Empleados;
+
+INSERT INTO Empleados (Nombre, Primer_apellido, Segundo_apellido, RFC, Puesto, Departamento, Fecha_Contratacion, Salario, Email, Telefono, Estado)
+VALUES
+();
